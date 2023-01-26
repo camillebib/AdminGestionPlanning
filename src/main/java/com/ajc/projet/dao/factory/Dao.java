8ed5c5a4063d7870b10ed2b601285a336469b918
Camille B <camille.bibaut0@gmail.com>
2023-01-26 16:11:15 +0100

    Ajout de la DAO, du loginServlet et des jsp de login

---
+++ src/main/java/com/ajc/projet/dao/factory/Dao.java
@@ -0,0 +1,12 @@
+package com.ajc.projet.dao.factory;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface Dao<T> {
+    Optional<T> get(Long id);   // Read : "SELECT ... FROM ... WHERE...
+    List<T> getAll();           //READ : "SELECT *
+    void create(T t);           //CREATE : INSERT INTO
+    void update(T t);           //UPDATE
+    void delete(T t);           //DELETE
+}
---
+++ src/main/java/com/ajc/projet/dao/factory/UserDAO.java
@@ -0,0 +1,131 @@
+package com.ajc.projet.dao.factory;
+
+import com.ajc.projet.dao.entities.User;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.TypedQuery;
+import jakarta.transaction.Transaction;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class UserDAO implements Dao<User> {
+
+    @Override
+    public Optional<User> get(Long id) {
+        Optional<User> result = Optional.empty();
+        EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
+        EntityTransaction et = em.getTransaction();
+        et.begin();
+        try {
+            result = Optional.of(em.find(User.class, id));
+            et.commit();
+        } catch (Exception e) {
+            if (et.isActive()) {
+                et.rollback();
+            }
+        } finally {
+            em.close();
+        }
+        return result;
+    }
+
+    @Override
+    public List<User> getAll() {
+        EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
+        EntityTransaction et = em.getTransaction();
+        List<User> users = new ArrayList<>();
+        et.begin();
+        try {
+            TypedQuery<User> usersQuery = em.createQuery("SELECT u FROM User u", User.class);
+            users = usersQuery.getResultList();
+            et.commit();
+        } catch (Exception e) {
+            if (et.isActive()) {
+                et.rollback();
+            }
+        } finally {
+            em.close();
+        }
+        return users;
+    }
+
+    @Override
+    public void create(User user) {
+        EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
+        EntityTransaction et = em.getTransaction();
+        et.begin();
+        try {
+            em.persist(user);
+            et.commit();
+        } catch (Exception e) {
+            if (et.isActive()) {
+                et.rollback();
+            }
+        } finally {
+            em.close();
+        }
+
+    }
+
+    @Override
+    public void update(User user) {
+        EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
+        EntityTransaction et = em.getTransaction();
+        et.begin();
+        try {
+            em.merge(user);
+            et.commit();
+        } catch (Exception e) {
+            if (et.isActive()) {
+                et.rollback();
+            }
+        } finally {
+            em.close();
+        }
+
+    }
+
+    @Override
+    public void delete(User user) {
+        EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
+        EntityTransaction et = em.getTransaction();
+        et.begin();
+        try {
+            em.remove(em.contains(user) ? user : em.merge(user));
+            et.commit();
+        } catch (Exception e) {
+            if (et.isActive()) {
+                et.rollback();
+            }
+        } finally {
+            em.close();
+        }
+
+    }
+
+    public boolean validate(String pseudo, String password) {
+
+        EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
+        EntityTransaction et = em.getTransaction();
+
+        User user = null;
+        et.begin();
+        try {
+            user = (User) em.createQuery("FROM User U WHERE U.pseudo = :pseudo").setParameter("pseudo", pseudo).getSingleResult();
+
+            if (user != null && user.getPassword().equals(password)) {
+                return true;
+            }
+            // commit transaction
+            et.commit();
+        } catch (Exception e) {
+            if (et.isActive()) {
+                et.rollback();
+            }
+            e.printStackTrace();
+        }
+        return false;
+    }
+}
