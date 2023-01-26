95fb74025d0258a81e645edadbe747f2754b5d0a
Camille B <camille.bibaut0@gmail.com>
2023-01-26 14:31:41 +0100

    Ajout des entités Admin et SuperAdmin

---
+++ src/main/java/com/ajc/projet/dao/entities/Admin.java
@@ -0,0 +1,10 @@
+package com.ajc.projet.dao.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+public class Admin extends User {
+
+
+}
---
+++ src/main/java/com/ajc/projet/dao/entities/SuperAdmin.java
@@ -0,0 +1,8 @@
+package com.ajc.projet.dao.entities;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class SuperAdmin extends Admin {
+
+}
